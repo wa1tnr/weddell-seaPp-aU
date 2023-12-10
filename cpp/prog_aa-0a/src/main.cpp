7bcfeeb7911bfed4a550a4ad09c5491872afde05
wa1tnr <wa1tnr@users.noreply.github.com>
2023-12-10 04:46:09 +0000

    swimming in it
    	modified:   error.log
    	modified:   src/main.cpp
    
    On branch dvlp-aa

--- cpp/prog_aa-0a/src/main.cpp
+++ cpp/prog_aa-0a/src/main.cpp
@@ -1,9 +1,9 @@
 // main.cpp
-// Sun 10 Dec 04:09:02 UTC 2023
+// Sun 10 Dec 04:43:10 UTC 2023
+
+// still sussing the bug/typo/&c 04:43z
 
 // add wizard++
-
-// not much happening but compiles clean
 
 #include <Arduino.h>
 #include <iostream>
@@ -11,9 +11,9 @@
 #include <string>
 
 template <typename... Args>
-std::string format_string(const std::string& format, Args... args) {
+std::string format_string(const std::string &format, Args... args) {
   int size_s = std::snprintf(nullptr, 0, format.c_str(), args...) +
-               1;  // Extra space for '\0'
+               1; // Extra space for '\0'
   if (size_s <= 0) {
     std::cout << "Error during formatting.";
     return "";
@@ -21,21 +21,17 @@ std::string format_string(const std::string& format, Args... args) {
   auto size = static_cast<size_t>(size_s);
   std::unique_ptr<char[]> buf(new char[size]);
   std::snprintf(buf.get(), size, format.c_str(), args...);
-  // return "";
-  return std::string(buf.get(), buf.get() + size - 1);  // We don't want the '\0' inside
+  return std::string(buf.get(),
+                     buf.get() + size - 1); // We don't want the '\0' inside
 }
 
 // Example of using above function to format a series of strings as a singular json formatted string.
-#if 0
  std::string json = format_string(
       "\"device_config\": {\"OTALogin\": \"%s\", \"OTAPassword\": \"%s\", "
       "\"OTAPort\": %u}",
       this->OTALogin.c_str(), this->OTAPassword.c_str(), this->OTAPort);
   return json;
-#endif
+ 
+ void setup(){}; void loop(){};
 
-// todo
-
-void setup(){};void loop(){};
-
-// end.
+ // end.
