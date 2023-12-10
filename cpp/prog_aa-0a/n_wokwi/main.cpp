8fe03c3ef32abb37cb61f9e0bb45a9e20a83bac4
wa1tnr <wa1tnr@users.noreply.github.com>
2023-12-10 03:50:29 +0000

    Full wokwi file set - very early
      almost nothing at all here ;)
    
    	new file:   diagram.json
    	new file:   main.cpp
    	new file:   sketch.ino
    	new file:   weddell-seaPp-aU_prog_aa-0a_r00aa-.zip
    	new file:   wokwi-project.txt
    
    On branch dvlp-aa

---
+++ cpp/prog_aa-0a/n_wokwi/main.cpp
@@ -0,0 +1,14 @@
+// main.cpp  10 Dec 2023 - new wokwi project
+
+// nick gammon - leave .ino empty .. that's it!
+
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Hello, ESP32!");
+}
+
+void loop() {
+  delay(10); // this speeds up the simulation
+}
